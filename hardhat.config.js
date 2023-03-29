bc8791f0de720723bd4438ad80a0929e500e974f
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-03-29 11:18:17 +0700

    chore: configure the hardhat network deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+const process = require("process");
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY]
+    }
+  }
 };
